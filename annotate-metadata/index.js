262397f6aa4d344900b3e368b047deb3f19ad6b4
Evan Anderson <evana@vmware.com>
2023-03-20 14:47:45 -0700

    Catch errors in function and print before crash

--- annotate-metadata/index.js
+++ annotate-metadata/index.js
@@ -1,4 +1,4 @@
- async function annotateImage(context, body) {
+async function annotateImage(context, body) {
   const image = body['spec']['imageConfig'][0];
 
   const imageData = image.config.config;
@@ -6,27 +6,32 @@
   console.log('Image data is: ', imageData);
   console.log('Labels are:', imageData.Labels);
 
-  let revision = imageData.Labels['org.opencontainers.image.revision'];
-  let label = imageData.Labels['org.opencontainers.image.version'];
-  let source = imageData.Labels['org.opencontainers.image.source'];
-  if (! revision) {
-    // Legacy format is e.g. "main/f11adae1c008aa8e381902fb7952686d0e7aac14" in the "source" field
-    // And no other keys from https://github.com/opencontainers/image-spec/blob/main/annotations.md#pre-defined-annotation-keys set
-    let legacy = imageData.Labels['org.opencontainers.image.source'];
-    [revision, label] = legacy.split('/');
-    source = body.spec.template.metadata['app.tanzu.vmware.com/source-url'];
-    if (! source) {
-      source = "unknown";
+  try {
+    let revision = imageData.Labels['org.opencontainers.image.revision'];
+    let label = imageData.Labels['org.opencontainers.image.version'];
+    let source = imageData.Labels['org.opencontainers.image.source'];
+    if (!revision) {
+      // Legacy format is e.g. "main/f11adae1c008aa8e381902fb7952686d0e7aac14" in the "source" field
+      // And no other keys from https://github.com/opencontainers/image-spec/blob/main/annotations.md#pre-defined-annotation-keys set
+      let legacy = imageData.Labels['org.opencontainers.image.source'];
+      console.log('On legacy path, splitting ', legacy);
+      [revision, label] = legacy.split('/');
+      source = body.spec.template.metadata['app.tanzu.vmware.com/source-url'];
+      if (!source) {
+        source = "unknown";
+      }
     }
-  }
 
-  body.status.template.metadata.annotations['app.tanzu.vmware.com/source-url'] = source;
-  body.status.template.metadata.annotations['app.tanzu.vmware.com/source-ref'] = label;
-  body.status.template.metadata.annotations['app.tanzu.vmware.com/source-hash'] = revision;
-  if (! body.status.appliedConventions) {
-    body.status.appliedConventions = [];
+    body.status.template.metadata.annotations['app.tanzu.vmware.com/source-url'] = source;
+    body.status.template.metadata.annotations['app.tanzu.vmware.com/source-ref'] = label;
+    body.status.template.metadata.annotations['app.tanzu.vmware.com/source-hash'] = revision;
+    if (!body.status.appliedConventions) {
+      body.status.appliedConventions = [];
+    }
+    body.status.appliedConventions.push('annotate-metadata');
+  } catch (error) {
+    console.log('Unable to enrich: ', error)
   }
-  body.status.appliedConventions.push('annotate-metadata');
 
   console.log('Returning: ', body);
 
